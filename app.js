9b711ed9ebfd3d9cf2f42c8da0583965a8d179a3
David <Rigby335@hotmail.com>
2022-04-27 11:24:39 -0700

    added eventlisteners

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const cheeseSlice = document.getElementById('cheese');
 const grapeVine = document.getElementById('grapes');
 const breadLoaf = document.getElementById('bread');
 const watermelonSlice = document.getElementById('watermelon');
+
+sodaPop.addEventListener('click', () => {
+
+});
+cheeseSlice.addEventListener('click', () => {
+
+});
+grapeVine.addEventListener('click', () => {
+
+});
+breadLoaf.addEventListener('click', () => {
+
+});
+watermelonSlice.addEventListener('click', () => {
+
+});
