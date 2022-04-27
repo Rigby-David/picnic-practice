9fba2d085a02c9e4c0da80d22890607dc05949e8
David <Rigby335@hotmail.com>
2022-04-27 11:19:31 -0700

    grabbed elements by id

---
+++ app.js
@@ -0,0 +1,5 @@
+const sodaPop = document.getElementById('soda');
+const cheeseSlice = document.getElementById('cheese');
+const grapeVine = document.getElementById('grapes');
+const breadLoaf = document.getElementById('bread');
+const watermelonSlice = document.getElementById('watermelon');
